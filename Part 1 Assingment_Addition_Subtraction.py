be130d0105065db38d625c8878dc0b072d1ae3a1
bhimanek <brian.himanek@csuglobal.edu>
2024-06-16 20:31:30 -0400

    Add files via upload

---
+++ Part 1 Assingment_Addition_Subtraction.py
@@ -0,0 +1,15 @@
+# Program to perform addition and subtraction of two numbers
+
+# Asking the user to input two numbers
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Calculating the addition
+addition = num1 + num2
+
+# Calculating the subtraction
+subtraction = num1 - num2
+
+# Displaying the results
+print("The addition of", num1, "and", num2, "is:", addition)
+print("The subtraction of", num1, "and", num2, "is:", subtraction)
